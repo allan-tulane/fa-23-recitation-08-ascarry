a3d7d2527ea57673eee796604c752d3664f7509a
ascarry <142826348+ascarry@users.noreply.github.com>
2023-12-06 17:15:05 +0000

    12/6 11:14am

--- answers.md
+++ answers.md
@@ -2,16 +2,16 @@
 
 ## Answers
 
-**Name:**_________________________
-**Name:**_________________________
+**Name:**___Abby Scarry__________
+**Name:**________________________
 
 
 Place all written answers from `recitation-08.md` here for easier grading.
 
 
 
-- **1b)**
-
+- **1b)** What is the work and span of your algorithm?
+Thw work and span of the algorithm is O(|E|log|E|) since it is Dijkstra's algorithm, with |E| being the number of edges.
 
 
 - **2b)**
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from collections import deque
-from heapq import heappush, heappop 
+import heapq
+from heapq import heappush, heappop
+
 
 def shortest_shortest_path(graph, source):
-    """
+  """
     Params: 
       graph.....a graph represented as a dict where each key is a vertex
                 and the value is a set of (vertex, weight) tuples (as in the test case)
@@ -12,37 +14,65 @@ def shortest_shortest_path(graph, source):
       a dict where each key is a vertex and the value is a tuple of
       (shortest path weight, shortest path number of edges). See test case for example.
     """
-    ### TODO
-    pass
-    
+  distances = {v: float('inf') for v in graph}
+  distances[source] = 0
+  edges = {v: float('inf') for v in graph}
+  edges[source] = 0
+  priority_queue = [(0, 0, source)]
 
-    
-    
+  while priority_queue:
+    current_distance, edge, current_vertex = heapq.heappop(priority_queue)
+    if current_distance > distances[current_vertex]:
+      continue
+    for neighbor, weight in graph[current_vertex]:
+      distance = current_distance + weight
+      if distance < distances[neighbor] or (distance == distances[neighbor]
+                                            and edge + 1 < edges[neighbor]):
+        distances[neighbor] = distance
+        edges[neighbor] = edge + 1
+        heapq.heappush(priority_queue, (distance, edge + 1, neighbor))
+  return {vertex: (distances[vertex], edges[vertex]) for vertex in graph}
+  pass
+
+
 def bfs_path(graph, source):
-    """
+  """
     Returns:
       a dict where each key is a vertex and the value is the parent of 
       that vertex in the shortest path tree.
     """
-    ###TODO
-    pass
+  queue = deque([source])
+  parents = {source: None}
+
+  while queue:
+    current_vertex = queue.popleft()
+
+    for neighbor in graph[current_vertex]:
+      if neighbor not in parents:
+        parents[neighbor] = current_vertex
+        queue.append(neighbor)
+
+  return parents
+  pass
+
 
 def get_sample_graph():
-     return {'s': {'a', 'b'},
-            'a': {'b'},
-            'b': {'c'},
-            'c': {'a', 'd'},
-            'd': {}
-            }
+  return {'s': {'a', 'b'}, 'a': {'b'}, 'b': {'c'}, 'c': {'a', 'd'}, 'd': {}}
 
 
-    
 def get_path(parents, destination):
-    """
+  """
     Returns:
       The shortest path from the source node to this destination node 
       (excluding the destination node itself). See test_get_path for an example.
     """
-    ###TODO
-    pass
+  path = []
+  current_node = destination
 
+  while current_node is not None:
+    path.append(current_node)
+    current_node = parents[current_node]
+
+  path = path[::-1][:-1]
+  return ''.join(path)
+  pass
